6074352eab7e44837e24d453b39e3cc35fa3cb68
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-01-04 16:40:11 -0300

    start env

--- db/migrations/20240104053155_add-session-id-to-transactions.ts
+++ db/migrations/20240104053155_add-session-id-to-transactions.ts
@@ -1,4 +1,3 @@
-import { table } from 'console'
 import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
+app.post('/hello', async (request, reply) => {
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+  return reply.send(transactions).code(201)
+})
+
 app.get('/hello', async (request, reply) => {
-  const tables = await knex('sqlite_schema').select()
-  return tables
+  const transactions = await knex('transactions').select()
+  return reply
+    .send({
+      transactions,
+    })
+    .code(201)
 })
 
 app
