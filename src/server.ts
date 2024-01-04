a0c882e83ded48ccdcd2e9bf1b22949f06e22602
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-01-04 16:50:02 -0300

    validate env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -26,7 +27,7 @@ app.get('/hello', async (request, reply) => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('🚀 HTTP Server running on port 3333 🚀')
