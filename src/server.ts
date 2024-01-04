0fc65f82dac01f1d16cabe670178920cee40399a
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-01-04 02:03:20 -0300

    install eslint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,10 @@ app.get('/hello', (request, reply) => {
   return reply.send({ message: 'Hello World' })
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('🚀 HTTP Server running on port 3333 🚀')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server running on port 3333 🚀')
+  })
