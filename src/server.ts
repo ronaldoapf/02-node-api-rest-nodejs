1baa7a2540adf9b351213ee4a89edd1162f6b1b3
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-01-05 16:37:22 -0300

    use plugins on fastify

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.post('/hello', async (request, reply) => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-  return reply.send(transactions).code(201)
-})
-
-app.get('/hello', async (request, reply) => {
-  const transactions = await knex('transactions').select()
-  return reply
-    .send({
-      transactions,
-    })
-    .code(201)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
